04da116ed24bcd27b246b93ea888c90723aaad5d
Josh Thomas <jctwake@gmail.com>
2018-02-02 14:29:24 -0500

    Mapbox setup and UnityGit test
    Working topo map
    Testing Unity.Github

---
+++ Assets/GUIDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIDisplay : MonoBehaviour {
+
+	public GUISkin GUISkinHelloWorld;
+
+	// Use this for initialization
+	void OnGUI () {
+
+		GUI.skin = GUISkinHelloWorld;
+
+		GUILayout.Button ("Hello World");
+
+	}
+	
+
+}
