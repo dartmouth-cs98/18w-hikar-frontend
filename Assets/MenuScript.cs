4747ecddfeb34892a888944dfcb43e43d3eefb1e
James Wang <james.k.wang.18@dartmouth.edu>
2018-05-24 12:09:58 -0400

    Exit hike function, preserving settings across runs, fixed the search map

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class MenuScript : MonoBehaviour {
 
 	public GameObject menuButton;
-	public GameObject MenuPanel;
+	public GameObject menuPanel;
+	public GameObject menuBlockPanel;
 	private Animator anim;
 	private bool isOpen = false;
 
@@ -18,31 +19,21 @@ public class MenuScript : MonoBehaviour {
 	public TransitionalObject panelTransitionObject;
 
 	void Start () {
-		//unpause the game on start
 		Time.timeScale = 1;
-		//get the animator component
-		anim = MenuPanel.GetComponent<Animator>();
-		//disable it on start to stop it from playing the default animation
-		anim.enabled = false;
-
 		if (uiObject != null) {
 			uiHandler = (UIManager)uiObject.GetComponent (typeof(UIManager));
 		}
 	}
 
-	// Update is called once per frame
-	public void Update () {
-		//pause the game on escape key press and when the game is not already paused
-		if(SwipeManager.Instance.isSwiping(SwipeDirection.Right) && !isOpen){
+	void Update () {
+		if (SwipeManager.Instance.isSwiping (SwipeDirection.Right) && !isOpen) {
 			OpenMenu ();
-			menuButton.SetActive (false);
+		} else if (SwipeManager.Instance.isSwiping (SwipeDirection.Left) && isOpen) {
+			CloseMenu ();
 		}
-		//unpause the game if its paused and the escape key is pressed
-		else if(SwipeManager.Instance.isSwiping(SwipeDirection.Left) && isOpen){
-			CloseMenu();
-		}
-		if(isOpen)
+		if (isOpen) {
 			uiHandler.userSelection ();
+		}
 	}
 
 	public void onClickHamburger() {
@@ -53,36 +44,26 @@ public class MenuScript : MonoBehaviour {
 		}
 	}
 
-	//function to pause the game
 	public void OpenMenu(){
-		//enable the animator component
-		//anim.enabled = true;
-		//play the Slidein animation
-		//anim.Play("MenuSlideIn");
-		//set the isPaused flag to true to indicate that the game is paused
 		if (uiHandler.isHiking) {
 			panelTransitionObject.TriggerFadeOut ();
 			StartCoroutine (waitPanel(0.6f));
 		}
 		menuTransitionObject.TriggerTransition();
-
 		isOpen = true;
-		//freeze the timescale
 		Time.timeScale = 0;
+		menuBlockPanel.gameObject.SetActive (true);
 	}
-	//function to unpause the game
+
 	public void CloseMenu(){
-		//set the isPaused flag to false to indicate that the game is not paused
 		isOpen = false;
-		//play the SlideOut animation
-		//anim.Play("MenuSlideOut");
 		menuTransitionObject.TriggerFadeOut();
 		if (uiHandler.isHiking && uiHandler.inAR) {
 			StartCoroutine (waitPanel(1f));
 			panelTransitionObject.TriggerTransition ();
 		}
-		//set back the time scale to normal time scale
 		Time.timeScale = 1;
+		menuBlockPanel.gameObject.SetActive (false);
 	}
 
 	public IEnumerator waitPanel(float wait){
