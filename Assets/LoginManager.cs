1f71100106e9ceb8b30ce59b533c2fa1aa0ec71a
Gregory Hunter <gregory.e.hunter.18@dartmouth.edu>
2018-05-29 01:49:19 -0400

    Sign up scene added , scripts added, and loading scene edited
    Added a switch from signin ( loadingScene) to SignUpScene. Created SignUpScene. Added preliminary scripts to handle login and signup along with swtching between them. Still need to switch to the topo scene on successful login and handle fail cases.

--- Assets/LoginManager.cs
+++ Assets/LoginManager.cs
@@ -12,6 +12,7 @@ public class LoginManager : MonoBehaviour {
     //  public GameObject loginObject;
     //  private LoginHandler loginHandler;
     public Button loginButton;
+    public Button SignUpButton;
     // Log in and Sign up
     public InputField usernameValue;
     public InputField PasswordValue;
@@ -26,16 +27,26 @@ public class LoginManager : MonoBehaviour {
             wwwScript = (WWWHandler)wwwHandler.gameObject.GetComponent(typeof(WWWHandler));
         }
         loginButton.onClick.AddListener(signInSubmit);
+        SignUpButton.onClick.AddListener(signUpSwitch);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         loginButton.onClick.AddListener(signInSubmit);
+        SignUpButton.onClick.AddListener(signUpSwitch);
 
 	}
     public void signInSubmit()
     {
-        Debug.Log("Got Here");
+        Debug.Log("Sending username and password to backened");
         StartCoroutine(wwwScript.PostSignIn(usernameValue.text, PasswordValue.text));
+        // TODO
+        // add success check // switch to topo scene
+        // also need to add text that only displays on failure
+    }
+    public void signUpSwitch()
+    {
+        Debug.Log("Trying to switch to SignUpScene");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName: "SignUpScene");
     }
 }
---
+++ Assets/SignUpManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using SimpleJSON;
+
+public class SignUpManager : MonoBehaviour {
+    //LoginHandler
+    //  public GameObject loginObject;
+    //  private LoginHandler loginHandler;
+    public Button signUpButton;
+    // Log in and Sign up
+    public InputField usernameValue;
+    public InputField PasswordValue;
+
+    //wwwHandler
+    public GameObject wwwHandler;
+    private WWWHandler wwwScript;
+    // Use this for initialization
+	// Use this for initialization
+	void Start () {
+        if (wwwHandler != null)
+        {
+            wwwScript = (WWWHandler)wwwHandler.gameObject.GetComponent(typeof(WWWHandler));
+        }
+        signUpButton.onClick.AddListener(signUpSubmit);
+	}
+	
+    // Update is called once per frame
+    void Update()
+    {
+        signUpButton.onClick.AddListener(signUpSubmit);
+
+    }
+    public void signUpSubmit()
+    {
+        Debug.Log("Registering username and password");
+        StartCoroutine(wwwScript.PostSignUp(usernameValue.text, PasswordValue.text));
+    }
+}
